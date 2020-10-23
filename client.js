86603b120c812a9c2f47cdfacd236d9028e9b6c4
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-10-23 17:41:59 +0200

    Initialize project
    Start with card-back-section containing some sample text

---
+++ client.js
@@ -0,0 +1,15 @@
+const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+
+window.TrelloPowerUp.initialize({
+  'card-back-section': function(t, options) {
+    return {
+      title: 'Test section',
+      icon: GRAY_ICON,
+      content: {
+        type: 'iframe',
+        url: t.signUrl('./section.html'),
+        height: 250
+      }
+    }
+  }
+});

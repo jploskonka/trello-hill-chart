130e1707abc29376285a90585750c702b6732dde
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-10-23 20:48:56 +0200

    chore: add Scope & Scope::Status models

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
-require "sinatra"
+require "sinatra/base"
 require 'sinatra/activerecord'
 
-class App < Sinatra::Base
+class Scope < ActiveRecord::Base
+  has_many :statuses, class_name: "Scope::Status"
+end
 
+class Scope::Status < ActiveRecord::Base
+  belongs_to :scope
+end
+
+class App < Sinatra::Application
+  post "/scopes/status" do # trello_card_id
+    scope = Scope.find_or_create_by(trello_card_id: params["trello_card_id"])
+    scope.statuses.create(progress: params["progress"])
+  end
 end

ddf74697b0db118173e66882b1ed71a3b5b45dd5
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-10-23 20:35:06 +0200

    db: add scope_statuses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_182130) do
+ActiveRecord::Schema.define(version: 2020_10_23_182352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "scope_statuses", force: :cascade do |t|
+    t.bigint "scope_id"
+    t.integer "progress", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["scope_id"], name: "index_scope_statuses_on_scope_id"
+  end
 
   create_table "scopes", force: :cascade do |t|
     t.string "trello_card_id", null: false
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_10_23_182130) do
     t.index ["trello_card_id"], name: "index_scopes_on_trello_card_id", unique: true
   end
 
+  add_foreign_key "scope_statuses", "scopes"
 end

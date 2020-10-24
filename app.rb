cb66dd500040f7be5520ee069722aaf41ad45083
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-10-24 14:07:51 +0200

    chore(app): add endpoint to get scope progress

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra/base"
 require 'sinatra/activerecord'
+require "sinatra/json"
 
 class Scope < ActiveRecord::Base
   has_many :statuses, class_name: "Scope::Status"
@@ -10,6 +11,11 @@ class Scope::Status < ActiveRecord::Base
 end
 
 class App < Sinatra::Application
+  get "/scopes/:trello_card_id" do
+    scope = Scope.find_by(trello_card_id: params["trello_card_id"])
+    json progress: scope.statuses.last.progress
+  end
+
   post "/scopes/status" do # trello_card_id
     scope = Scope.find_or_create_by(trello_card_id: params["trello_card_id"])
     scope.statuses.create(progress: params["progress"])

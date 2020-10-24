30d019b4192900a0e54d14b676ca3e5366a005c8
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-10-24 12:43:27 +0200

    feat(trello): Change section title

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-
 window.TrelloPowerUp.initialize({
   'card-back-section': function(t, options) {
     return {
-      title: 'Test section',
+      title: 'Scope progress',
       icon: GRAY_ICON,
       content: {
         type: 'iframe',

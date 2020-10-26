0543d473a5dc1b4bb23a769b639a942a14fd57a4
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-10-26 12:28:56 +0100

    feat(badges): add colors depending on progress

--- client.js
+++ client.js
@@ -17,19 +17,21 @@ window.TrelloPowerUp.initialize({
       .get('id')
       .then(function(cardId) {
         return fetch(`https://trello-hill-chart.herokuapp.com/scopes/${cardId}`)
+          .then(function(response) { return response.json(); })
           .then(function(response) {
-            console.log('parsing response')
-            return response.json();
-          })
-          .then(function(response) {
-            console.log('response received!', cardId, response, response.progress)
             const text = response.progress < 10 ? 'warming up'
               : response.progress >= 10 && response.progress < 50 ? 'figuring out'
               : response.progress >= 50 && response.progress < 90 ? 'making it'
               : 'almost there'
 
+            const color = response.progress < 10 ? 'gray'
+              : response.progress >= 10 && response.progress < 50 ? 'orange'
+              : response.progress >= 50 && response.progress < 90 ? 'yellow'
+              : 'lime'
+
             return [{
-              text: text
+              text: text,
+              color: color
             }]
           })
       })

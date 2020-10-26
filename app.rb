38a65253eac64c1ccd5c3fb3bac6b1aea2e5e813
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-10-26 12:18:56 +0100

    chore(app): add Accept-Control-Allow-Origin header

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra/base"
 require 'sinatra/activerecord'
 require "sinatra/json"
+require "sinatra/cross_origin"
 
 class Scope < ActiveRecord::Base
   has_many :statuses, class_name: "Scope::Status"
@@ -11,7 +12,13 @@ class Scope::Status < ActiveRecord::Base
 end
 
 class App < Sinatra::Application
-  disable :protection
+  configure do
+    enable :corss_origin
+  end
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = "*"
+  end
 
   get "/scopes/:trello_card_id" do
     if scope = Scope.find_by(trello_card_id: params["trello_card_id"])

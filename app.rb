a3d9c029a36aca371359e7dcf357b8c0b6313315
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-10-26 11:59:22 +0100

    chore(chart): don’t use jsonp to fetch data

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require "sinatra/base"
 require 'sinatra/activerecord'
-require "sinatra/jsonp"
+require "sinatra/json"
 
 class Scope < ActiveRecord::Base
   has_many :statuses, class_name: "Scope::Status"
@@ -11,11 +11,12 @@ class Scope::Status < ActiveRecord::Base
 end
 
 class App < Sinatra::Application
+  set :protection, except: [:json_csrf]
   get "/scopes/:trello_card_id" do
     if scope = Scope.find_by(trello_card_id: params["trello_card_id"])
-      jsonp progress: scope.statuses.last&.progress
+      json progress: scope.statuses.last&.progress
     else
-      jsonp progress: 0
+      json progress: 0
     end
   end
 

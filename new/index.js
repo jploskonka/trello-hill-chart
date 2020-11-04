2bf0d5d9cbff650e0d6db40acf8e2249456e0113
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-11-04 21:03:16 +0100

    wip

---
+++ new/index.js
@@ -0,0 +1,40 @@
+const ICON = 'https://jploskonka.github.io/trello-hill-chart/icon.svg';
+
+window.TrelloPowerUp.initialize({
+  'card-back-section': function(t, options) {
+    return {
+      title: 'How’s it going?',
+      icon: ICON,
+      content: {
+        type: 'iframe',
+        url: t.signUrl('./new_section.html'),
+        height: 250
+      }
+    }
+  }
+});
+  // 'card-badges': function(t, options) {
+  //   return t.card('id')
+  //     .get('id')
+  //     .then(function(cardId) {
+  //       return fetch(`https://trello-hill-chart.herokuapp.com/scopes/${cardId}`)
+  //         .then(function(response) { return response.json(); })
+  //         .then(function(response) {
+  //           const text = response.progress < 10 ? 'warming up'
+  //             : response.progress >= 10 && response.progress < 50 ? 'uncertain'
+  //             : response.progress >= 50 && response.progress < 90 ? 'certain'
+  //             : response.progress < 96 ? 'almost there' : 'done'
+  //
+  //           const color = response.progress < 10 ? 'gray'
+  //             : response.progress >= 10 && response.progress < 50 ? 'orange'
+  //             : response.progress >= 50 && response.progress < 90 ? 'yellow'
+  //             : response.progress < 96 ? 'lime' : 'green'
+  //
+  //           return [{
+  //             text: text,
+  //             color: color
+  //           }]
+  //         })
+  //     })
+  // }
+// });
